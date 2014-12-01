8b5c7e4ebb1462ffdec342df9a59a86b13b2d66f
LinkedHK <henryproface@gmail.com>
2014-12-01 20:59:59 +0800

    Added Categories Browser

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,9 +8,27 @@
       	[:id, :item_id,:location_district_id,:location_city_id,:d_coord_lat,:d_coord_long ] }
       	 })
     end
+
+    def browse_by
+      #information-technology
+      @item =  Item.browse(params['category'],params['page'], 10)
+      render :json =>  @item.as_json(include:
+                                         { item_descriptions:{ :only =>[:item_title]},
+                                           item_location:{ :except =>
+                                                               [:id, :item_id,:location_district_id,:location_city_id,:d_coord_lat,:d_coord_long ] }
+                                         })
+    end
+
+    def list_cat
+      @cats = CategoryDescription.where(:locale => 'en_US').joins(:item_category)
+                  .select('category_description.item_category_id as id,category_description.name, item_categories.items_count')
+      render :json =>  @cats.as_json
+    end
+
+
+
    def show_item
      @item = ItemPresenter.new(Item.where(id: show_params[:id]).first)
-
      render :json =>  {
                 :id => @item.id,
                 :title=> @item.title,
@@ -26,5 +44,9 @@
     def show_params
         params.permit(:id)
     end
+
+   def browse_by_params
+     params.permit(:id, :category, :page)
+   end
 
   end
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
      resource :items
     get '/show_item',:to => "items#show_item" #,constraints: { id: /[a-zA-Z0-9_-]+/ }
+    get '/browse_by',:to => "items#browse_by" #,constraints: { id: /[a-zA-Z0-9_-]+/ }
+    get '/list_cat',:to => "items#list_cat" #,constraints: { id: /[a-zA-Z0-9_-]+/ }
     end
   end
 end
